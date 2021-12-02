81b3cb299da77431193dab97ada229546be79cd8
Shyngyskhan Oralkhanov <Shyngyskhan_Oralkhanov@epam.com>
2021-12-02 12:05:02 +0600

    Equals ovveride added

--- Homework2. Debugging fundamentals/Task1/Product.cs
+++ Homework2. Debugging fundamentals/Task1/Product.cs
@@ -11,5 +11,17 @@
         public string Name { get; set; }
 
         public double Price { get; set; }
+        public override bool Equals(object obj)
+        {
+            var other = obj as Product;
+
+            if (other == null)
+                return false;
+
+            if (Name != other.Name || Price != other.Price)
+                return false;
+
+            return true;
+        }
     }
 }
